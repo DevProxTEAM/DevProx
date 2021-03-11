b8c91c3383827ba44b521c787d69c1ca6df1afda
DevProxTEAM <80484138+DevProxTEAM@users.noreply.github.com>
2021-03-11 23:26:00 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -9511,7 +9511,7 @@ if SecondSudo(msg) then
 if text == "تحديث السورس" or text == "تحديث سورس" then 
 Dev_Abs(msg.chat_id_, msg.id_, 1, '⌁︙جاري تحديث سورس ديف بروكس', 1, 'md') 
 os.execute('rm -rf DevProx.lua') 
-os.execute('wget https://raw.githubusercontent.com/iq0abs/DevProx/master/DevProx.lua') 
+os.execute('wget https://raw.githubusercontent.com/DevProxTEAM/DevProx/master/DevProx.lua') 
 dofile('DevProx.lua') 
 io.popen("rm -rf ../.telegram-cli/*")
 print("\27[31;47m\n          ( تم تحديث السورس )          \n\27[0;34;49m\n") 
@@ -9601,7 +9601,7 @@ end
 send(msg.chat_id_, msg.id_,Files)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/iq0abs/DevProxFiles/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/DevProxTEAM/DevProxFiles/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -9633,7 +9633,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/iq0abs/DevProxFiles/master/DevProxFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/DevProxTEAM/DevProxFiles/master/DevProxFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n⌁︙الملف ↫ *"..FileName.."*\n⌁︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -9645,7 +9645,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/iq0abs/DevProxFiles/master/DevProxFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/DevProxTEAM/DevProxFiles/master/DevProxFiles/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
