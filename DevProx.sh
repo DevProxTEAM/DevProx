e2170690b43e83e2e359ab617b805527f947dc08
DevProxTEAM <80484138+DevProxTEAM@users.noreply.github.com>
2021-03-18 12:36:32 +0300

    Update DevProx.sh

--- DevProx.sh
+++ DevProx.sh
@@ -16,7 +16,7 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y curl -y htop -y wget -y
 if [ "$Version" == "18" ]; then
-echo -e "\033[0;31m\n~ Installing Depedencies For Ubuntu 18... \n\033[0;33m"
+echo -e "\033[0;31m\n~ Installing Depedencies For Ubuntu 18... \n\033[0m"
 cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
 wget "apiabs.ml/installubuntu18/compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
